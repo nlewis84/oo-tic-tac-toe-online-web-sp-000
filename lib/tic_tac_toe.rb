f8dcd1c0142b4744f0a7dac0a6c08ca9fd5e4354
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-29 05:14:28 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,106 @@
+require 'pry'
+
+class TicTacToe
+
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
+   
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(string)
+    string_integer = string.to_i
+    string_integer -= 1
+  end
+  
+  def move(index, player = "X")
+    @board[index] = player
+  end
+  
+  def position_taken?(index)
+    @board[index] != " "
+  end
+  
+  def valid_move?(index)
+    index >= 0 && index <= 9 && !position_taken?(index)
+  end
+  
+  def turn_count
+    @board.count {|i| ["X", "O"].include? i }
+  end
+  
+  def current_player
+    if turn_count % 2 == 0  
+      "X"
+    else 
+      "O"
+    end
+  end
+  
+  def turn
+    puts "Enter your move by entering a number 1-9:"
+    move = gets.strip
+    index = input_to_index(move)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+      turn
+    end
+  end
+  
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
+    end
+  end
+  
+  def full?
+    @board.all?{|token| token == "X" || token == "O"}
+  end
+  
+  def draw?
+    full? && !won?
+  end
+  
+  def over?
+    won? || draw?
+  end
+  
+  def winner
+    if winning_combo = won?
+      @board[winning_combo.first]
+    end
+  end
+  
+  def play
+    until over?
+      turn
+    end 
+    if won?
+      winner == "X" || winner == "O" 
+      puts "Congratulations #{winner}!" 
+    elsif draw?
+      puts "Cat's Game!" 
+    end 
+  end
+end
